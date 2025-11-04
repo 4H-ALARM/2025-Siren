63522c3cbc9c6242206d70a5f556a5165e72b745
Aditya Naravane <adityanaravane2008@gmail.com>
2025-11-04 18:50:07 -0500

    not stubbed yet, need to stub

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,8 +13,6 @@
 
 package frc.robot;
 
-import static frc.lib.constants.VisionConstants.*;
-
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -23,38 +21,8 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.lib.constants.SwerveConstants;
-import frc.lib.enums.LevelEnum;
-import frc.lib.enums.robotStates;
-import frc.robot.commands.CommandGroups.DeAlgifyCommand;
-import frc.robot.commands.CommandGroups.IntakeCommandGroup;
-import frc.robot.commands.CommandGroups.ScoreCommandGroup;
-import frc.robot.commands.DriveCommands;
-import frc.robot.commands.StateCommands.IntakeAlgae;
-import frc.robot.commands.StateCommands.PlaceAtChosenHeight;
-import frc.robot.commands.StateCommands.ThrowAlgae;
-import frc.robot.subsystems.bargemech.bargeIONeo;
-import frc.robot.subsystems.bargemech.bargeMech;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.GyroIO;
-import frc.robot.subsystems.drive.GyroIOPigeon2;
-import frc.robot.subsystems.drive.ModuleIO;
-import frc.robot.subsystems.drive.ModuleIOSim;
-import frc.robot.subsystems.drive.ModuleIOTalonFX;
-import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorIOKraken;
-import frc.robot.subsystems.endeffector.ClawIOVortex;
-import frc.robot.subsystems.endeffector.EndEffector;
-import frc.robot.subsystems.endeffector.WristIONeo;
-import frc.robot.subsystems.groundintake.GroundIntake;
-import frc.robot.subsystems.groundintake.GroundIntakeIOFalconVortex;
-import frc.robot.subsystems.virtualsubsystems.statehandler.StateHandler;
-import frc.robot.subsystems.vision.Vision;
-import frc.robot.subsystems.vision.VisionIO;
-import frc.robot.subsystems.vision.VisionIOLimelight;
-import frc.robot.subsystems.vision.VisionIOPhotonVision;
-import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -65,174 +33,34 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
   // Subsystems
-  private final StateHandler stateHandler = new StateHandler();
-  private final Drive drive;
-  private final Vision vision;
-  private final Elevator elevator;
-  private final EndEffector endEffector;
-  private final GroundIntake groundIntake;
-  private final bargeMech barge;
-
-  private final ToggleHandler elevatorDisable = new ToggleHandler("elevatorDisable");
-  private final ToggleHandler alignDisable = new ToggleHandler("alignDisable");
 
   // Controller
-  private final CommandXboxController pilot = new CommandXboxController(0);
-  private final CommandXboxController copilot = new CommandXboxController(1);
+  private final CommandXboxController pilot = new CommandXboxController(0);it
 
   // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
 
-  private final IntakeCommandGroup intake;
-  private final ScoreCommandGroup score;
-  private final ScoreCommandGroup score2;
-  private final PlaceAtChosenHeight placeAtChosenHeight;
-  private final IntakeAlgae intakeAlgae;
-  private final ThrowAlgae throwAlgae;
-  private final DeAlgifyCommand deAlgifyCommandGroup;
-
-  // private final AlignToPoseCommand driveToPose;
+  // Commands
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     switch (Constants.currentMode) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(SwerveConstants.FrontLeft),
-                new ModuleIOTalonFX(SwerveConstants.FrontRight),
-                new ModuleIOTalonFX(SwerveConstants.BackLeft),
-                new ModuleIOTalonFX(SwerveConstants.BackRight));
-
-        vision =
-            new Vision(
-                drive::addVisionMeasurement,
-                new VisionIOPhotonVision(camera1Name, robotToCamera1),
-                new VisionIOPhotonVision(camera2Name, robotToCamera2),
-                // new VisionIOPhotonVision(camera3Name, robotToCamera3),
-                // new VisionIOPhotonVision(camera4Name, robotToCamera4),
-                new VisionIOLimelight(limelightName, drive::getRotation));
-
-        elevator = new Elevator(new ElevatorIOKraken(), stateHandler);
-
-        groundIntake = new GroundIntake(new GroundIntakeIOFalconVortex(), stateHandler);
-
-        endEffector = new EndEffector(new ClawIOVortex(), new WristIONeo(), stateHandler);
-
-        barge = new bargeMech(new bargeIONeo(), stateHandler);
+        
 
         break;
 
       case SIM:
         // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(SwerveConstants.FrontLeft),
-                new ModuleIOSim(SwerveConstants.FrontRight),
-                new ModuleIOSim(SwerveConstants.BackLeft),
-                new ModuleIOSim(SwerveConstants.BackRight));
-
-        vision =
-            new Vision(
-                drive::addVisionMeasurement,
-                new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, drive::getPose),
-                new VisionIOPhotonVisionSim(camera2Name, robotToCamera2, drive::getPose) // ,
-                // new VisionIOPhotonVisionSim(camera3Name, robotToCamera3, drive::getPose),
-                // new VisionIOPhotonVisionSim(camera4Name, robotToCamera4, drive::getPose)
-                );
-
-        elevator = new Elevator(new ElevatorIOKraken(), stateHandler);
-
-        groundIntake = new GroundIntake(new GroundIntakeIOFalconVortex(), stateHandler);
-
-        endEffector = new EndEffector(new ClawIOVortex(), new WristIONeo(), stateHandler);
-
-        barge = new bargeMech(new bargeIONeo(), stateHandler);
+        
 
         break;
 
       default:
         // Replayed robot, disable IO implementations
 
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-
-        vision =
-            new Vision(
-                drive::addVisionMeasurement,
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {});
-
-        elevator = new Elevator(new ElevatorIOKraken(), stateHandler);
-
-        groundIntake = new GroundIntake(new GroundIntakeIOFalconVortex(), stateHandler);
-
-        endEffector = new EndEffector(new ClawIOVortex(), new WristIONeo(), stateHandler);
-
-        barge = new bargeMech(new bargeIONeo(), stateHandler);
-
         break;
     }
-
-    intake =
-        new IntakeCommandGroup(
-            drive, elevator, endEffector, groundIntake, stateHandler, elevatorDisable);
-    score =
-        new ScoreCommandGroup(
-            drive,
-            elevator,
-            endEffector,
-            groundIntake,
-            stateHandler,
-            elevatorDisable,
-            alignDisable);
-
-    score2 =
-        new ScoreCommandGroup(
-            drive,
-            elevator,
-            endEffector,
-            groundIntake,
-            stateHandler,
-            elevatorDisable,
-            alignDisable);
-    placeAtChosenHeight =
-        new PlaceAtChosenHeight(elevator, endEffector, stateHandler, elevatorDisable);
-    intakeAlgae = new IntakeAlgae(groundIntake, stateHandler);
-    throwAlgae = new ThrowAlgae(groundIntake, stateHandler);
-    deAlgifyCommandGroup =
-        new DeAlgifyCommand(
-            drive,
-            elevator,
-            endEffector,
-            groundIntake,
-            barge,
-            stateHandler,
-            elevatorDisable,
-            alignDisable);
-
-    NamedCommands.registerCommand("Score", score2);
-    NamedCommands.registerCommand("Intake", intake);
-    NamedCommands.registerCommand(
-        "setL4", Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.L4)));
-
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-
-    // Set up auto routines
-    var defaultAuto = DriveCommands.driveBackwards(drive).withTimeout(5);
-    autoChooser.addDefaultOption("leave", defaultAuto);
-    addAuto("leave", defaultAuto);
 
     configureButtonBindings();
   }
@@ -246,59 +74,7 @@ public class RobotContainer {
   private void configureButtonBindings() {
     // Default command, normal field-relative drive
 
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> (-pilot.getLeftY()),
-            () -> (-pilot.getLeftX()),
-            () -> (pilot.getRightX())));
-
-    // Reset gyro to 0° when B button is pressed
-    pilot
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
-                .ignoringDisable(true));
-
-    pilot.leftTrigger().toggleOnTrue(intake);
-    // pilot.leftBumper().whileTrue(deAlgifyCommandGroup);
-
-    // elevator.setDefaultCommand(
-    //     Commands.run(
-    //         () -> elevator.moveElevator(copilot.getLeftY()),
-    //         elevator)); // Set elevator to bottom position on startup
-
-    copilot.a().onTrue(Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.L1)));
-    copilot.b().onTrue(Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.L2)));
-    copilot.y().onTrue(Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.L3)));
-    copilot.x().onTrue(Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.L4)));
-    copilot
-        .leftTrigger()
-        .onTrue(
-            Commands.runOnce(() -> stateHandler.setState(robotStates.RESTINGPOSTDEALGIFYNOHOLD)));
-    copilot
-        .leftBumper()
-        .onTrue(Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.DEALGIFYLOW)));
-    copilot
-        .rightBumper()
-        .onTrue(Commands.runOnce(() -> stateHandler.setLevelEnum(LevelEnum.DEALGIFYHIGH)));
-
-    copilot.povLeft().onTrue(Commands.runOnce(() -> elevatorDisable.toggle()));
-    copilot.povUp().onTrue(Commands.runOnce(() -> alignDisable.toggle()));
-
-    pilot.rightTrigger().whileTrue(score);
-    pilot.rightBumper().onTrue(placeAtChosenHeight.withTimeout(1));
-    pilot.leftBumper().whileTrue(intakeAlgae);
-    pilot.x().whileTrue(throwAlgae);
-  }
-
-  public void addAuto(String name, Command command) {
-    NamedCommands.registerCommand(name, command);
-    autoChooser.addOption(name, command);
+    
   }
 
   /**
@@ -308,6 +84,6 @@ public class RobotContainer {
    *     <p>n in autonomous
    */
   public Command getAutonomousCommand() {
-    return autoChooser.get();
+    return new InstantCommand();
   }
 }
